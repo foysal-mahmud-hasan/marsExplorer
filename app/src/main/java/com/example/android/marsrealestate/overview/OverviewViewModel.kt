00e92cb4b05ba72478f9ddb7ae432cef230b1de6
TatsuSolo <1730150@iub.edu.bd>
2023-01-16 19:02:11 +0600

    Office-init-1-16-23

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,16 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import okhttp3.Dispatcher
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -36,6 +46,10 @@ class OverviewViewModel : ViewModel() {
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
     init {
         getMarsRealEstateProperties()
     }
@@ -44,6 +58,18 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        coroutineScope.launch {
+            MarsApi.retrofitService.getProperties().enqueue(object : Callback<List<MarsProperty>>{
+                override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                    _response.value = "Success : ${response.body()?.size} Mars properties retrieved"
+                }
+
+                override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+                    _response.value = "Failure : " + t.message
+                }
+            })
+        }
+
+        _response.value = "Set the Mars API Response here !"
     }
 }
